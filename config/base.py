6b68565d127a558f2a9842c74eff1699d5828abd
we684123 <we684123@gmail.com>
2022-10-02 23:34:30 +0800

    🔧 base 小修

--- config/base.py
+++ config/base.py
@@ -1,7 +1,7 @@
 def logger_config():
     return {
-        "logging_level": "INFO",  # DEBUG # INFO # ERROR # WARNING
-        "log_file_path": './logs/example',
+        "logging_level": "DEBUG",  # DEBUG # INFO # ERROR # WARNING
+        "log_file_path": './logs/example.log',
         "log_format": '%(asctime)s - %(levelname)s : %(message)s',
         "backupCount": 7,
         "when": 'D',
