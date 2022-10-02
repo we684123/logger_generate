a816f1b056f48ab3eceb87712d36ec9f728bd116
we684123 <we684123@gmail.com>
2022-10-02 23:37:12 +0800

    🐛✨💄 修正多層 log 資料夾無法建立 + 改成 '引入即用' 更方便 + 改良 logger 名稱

--- library/logger_generate.py
+++ library/logger_generate.py
@@ -7,12 +7,12 @@ from pathlib import Path
 import coloredlogs
 
 
-def generate(logger_config, name='', need_serial=False, **kwargs):
+def generate(logger_config=None, name='', need_serial=False, **kwargs):
     """
     logger_config is like to
     {
         "logging_level": "INFO",  # DEBUG # INFO # ERROR # WARNING
-        "log_file_path": './logs/example',
+        "log_file_path": './logs/{folderName}.log',
         "log_format": '%(asctime)s - %(levelname)s : %(message)s',
         "backupCount": 7,
         "when": 'D',
@@ -23,17 +23,37 @@ def generate(logger_config, name='', need_serial=False, **kwargs):
     if True, will generate have serial logger
     """
 
+    folderName = Path().absolute().parts[-1]
+    if logger_config is None:
+        # 如果沒有給 logger_config
+        if 'logging_level' in kwargs:
+            logging_level = kwargs['logging_level']
+        else:
+            logging_level = "INFO"
+
+        logger_config = {
+            "logging_level": logging_level,
+            "log_file_path": f'./logs/{folderName}.log',
+            "log_format": '%(asctime)s - %(levelname)s : %(message)s',
+            "backupCount": 7,
+            "when": 'D',
+            "encoding": 'utf-8',
+        }
+
     if 'need_serial' in kwargs:
         need_serial = kwargs['need_serial']
 
-    if not Path(logger_config['log_file_path']).exists():
-        try:
-            Path(logger_config['log_file_path']).parents[0].mkdir()
-        except Exception as e:
-            print(e)
+    log_file_path = Path(logger_config['log_file_path']).absolute()
+    parts = log_file_path.parts
+    parts_inherit = Path().absolute()
+    for part in parts[:-1]:
+        # 除了最後一個是log檔，其他的上層資料夾都要確保存在
+        if not (parts_inherit := parts_inherit / part).exists():
+            # 資料夾不存在就建
+            parts_inherit.mkdir()
 
     if name == '':
-        name = __name__
+        name = folderName
     if need_serial:
         rdt_len = 5
         rdt = ''.join(random.choice(string.ascii_letters + string.digits)
