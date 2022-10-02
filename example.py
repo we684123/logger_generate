2251e3244e480c593fd4947c5a1eafcce631040c
we684123 <we684123@gmail.com>
2022-10-02 23:39:45 +0800

    🚸 改善範例程式

--- example.py
+++ example.py
@@ -1,20 +1,25 @@
 from config import base
 from library.logger_generate import generate
 
-logger = generate(base.logger_config())
-logger.info("A")
+logger = generate()
+logger.info("引入即用 ( •̀ ω •́ )✧")
 
-logger = generate(base.logger_config(), "ex_B")
-logger.info("B")
+logger_config = {
+    "logging_level": 'DEBUG',
+    "log_file_path": './logs/by_logger_config.log',
+    "log_format": '%(asctime)s - %(levelname)s : %(message)s',
+    "backupCount": 7,
+    "when": 'D',
+    "encoding": 'utf-8',
+}
+logger = generate(logger_config, name='from_dict')
+logger.info("亦可用程式內 dict 設定")
 
-logger = generate(base.logger_config(), name='ex_C')
-logger.info("C")
+logger = generate(base.logger_config(), "from_config_file")
+logger.info("也以用從檔案引入 config")
+
+logger = generate(logger_config, name='方便生成隨機log名稱', need_serial=True)
+logger.info("←後方隨機5字元")
 
 logger = generate(base.logger_config(), "ex", True)
-logger.info("D")
-
-logger = generate(base.logger_config(), need_serial=True)
-logger.info("E")
-
-logger = generate(base.logger_config(), name='ex_F', need_serial=True)
-logger.info("F")
+logger.info("示範單純用位置作為輸入手段")
