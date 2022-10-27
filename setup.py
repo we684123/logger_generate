cfcd7356173b79fea54ab8fcfa2f5b3dfd9c1b5b
we684123 <we684123@gmail.com>
2022-10-28 01:09:23 +0800

    💩

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="logger_generate",
-    version="0.1.0",
+    version="0.1.3",
     keywords=("logger", "logging", "generate"),
     description="開箱即用的logger。 easy to generate logger.",
     long_description=long_description,
